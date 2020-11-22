302e1cffa3d7e8bd7f8686aeeca4db59709bc0ad
Odai Athamneh <heyodai@gmail.com>
2020-11-21 22:42:34 -0600

    uploading skeleton for calculate-the-profit.py

---
+++ python/medium/calculate-the-profit.py
@@ -0,0 +1,12 @@
+# Edabit "Calculate the Profit" Challenge
+# @url https://edabit.com/challenge/YfoKQWNeYETb9PYpw
+# @author Odai Athamneh
+# @date 2020/11/21
+
+def main():
+    return
+
+def profit(info):
+    return
+
+main()
