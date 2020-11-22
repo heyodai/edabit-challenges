7989ac99fcd9fee31cd8b3c259f02bb82a727f1e
Odai Athamneh <heyodai@gmail.com>
2020-11-21 22:53:01 -0600

    uploading working prototype for calculate-the-profit.py

--- python/medium/calculate-the-profit.py
+++ python/medium/calculate-the-profit.py
@@ -4,9 +4,25 @@
 # @date 2020/11/21
 
 def main():
+    # test the profit() function
+    print(profit({
+        "cost_price": 32.67,
+        "sell_price": 45.00,
+        "inventory": 1200
+    })) # Expected output: 14796
+    print(profit({
+        "cost_price": 225.89,
+        "sell_price": 550.00,
+        "inventory": 100
+    })) # Expected output: 32411
+    print(profit({
+        "cost_price": 2.77,
+        "sell_price": 7.95,
+        "inventory": 8500
+    })) # Expected output: 44030
     return
 
 def profit(info):
-    return
+    return (info["sell_price"] - info["cost_price"]) * info["inventory"]
 
 main()
