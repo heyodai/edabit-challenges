a30f575a1039521f8149a336d41a5956a7f59629
Odai Athamneh <heyodai@gmail.com>
2020-11-21 19:12:10 -0600

    created basic for loop in fizzbuzz.py

---
+++ python/medium/fizzbuzz.py
@@ -0,0 +1,14 @@
+# Edabit Fizzbuzz Challenge
+# @url https://edabit.com/challenge/WXqH9qvvGkmx4dMvp
+# @author Odai Athamneh
+# @date 2020/11/21
+
+def fizz_buzz(num):
+    for i in range(1, num + 1):
+        print(i)
+
+# test the output
+fizz_buzz(3) # "Fizz"
+fizz_buzz(5) # "Buzz"
+fizz_buzz(15) # "FizzBuzz"
+fizz_buzz(4) # "4"
